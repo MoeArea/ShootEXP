63b45387a1ff331096b3ee4d7d2ec2ce2a55226a
R-Josef <460257626@qq.com>
2023-09-09 01:19:51 +0800

    兼容酿酒插件"Brewery"

--- src/main/java/moe/feo/shootexp/EatListener.java
+++ src/main/java/moe/feo/shootexp/EatListener.java
@@ -1,15 +1,17 @@
 package moe.feo.shootexp;
 
+import com.dre.brewery.api.BreweryApi;
 import moe.feo.shootexp.config.Config;
 import moe.feo.shootexp.config.Language;
-import org.bukkit.Bukkit;
-import org.bukkit.SoundCategory;
+import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+
+import static org.bukkit.Bukkit.getServer;
 
 /**
  * 监听玩家右键
@@ -27,6 +29,21 @@ public class EatListener implements Listener {
 		if (action == Action.RIGHT_CLICK_AIR || action == Action.RIGHT_CLICK_BLOCK) {
 			if (!EXP.isEXPItem(item)) {
 				return;
+			}
+			if (getServer().getPluginManager().getPlugin("Brewery").isEnabled() && action == Action.RIGHT_CLICK_BLOCK) {
+				System.out.println("Material: " + e.getClickedBlock().getType().name());
+				if (BreweryApi.getBarrel(e.getClickedBlock()) != null || BreweryApi.getCauldron(e.getClickedBlock()) != null) {
+					return;
+				} else if (e.getClickedBlock().getType() == Material.getMaterial("WATER_CAULDRON")) {
+					int x = e.getClickedBlock().getLocation().getBlockX();
+					int y = e.getClickedBlock().getLocation().getBlockY();
+					int z = e.getClickedBlock().getLocation().getBlockZ();
+					World world = e.getClickedBlock().getLocation().getWorld();
+					Location down = new Location(world, x, y-1, z);
+					if (down.getBlock().getType() == Material.FIRE) {
+						return;
+					}
+				}
 			}
 			EXP exp = new EXP(item);
 			switch (Config.EXP_TYPE.getString()){
@@ -55,7 +72,7 @@ public class EatListener implements Listener {
 					recipient.sendMessage(msg);
 				}
 			} else {
-				Bukkit.getServer().broadcastMessage(msg);
+				getServer().broadcastMessage(msg);
 			}
 			item.setAmount(0);
 			e.setCancelled(true);
